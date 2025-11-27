ce0601389bba86fc24fc74eea4445a831a4f6270
TXAVL <102934765+TXAVL@users.noreply.github.com>
2025-11-27 21:56:44 +0700

    Fix encoding issue and streamline download scripte

--- download.ps1
+++ download.ps1
@@ -1,8 +1,9 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $exePath = "$env:TEMP\Unikey-TXA.exe"
 $exeUrl = "https://raw.githubusercontent.com/TXAVL/TXAOPTIMIZE/main/Unikey%20-%20TXA.exe"
 
 Write-Output "Đang tải file..."
-Invoke-WebRequest -Uri $exeUrl -OutFile $exePath -UseBasicParsing
+Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
 Write-Output "Tải thành công! Đang chạy..."
 Start-Process $exePath
