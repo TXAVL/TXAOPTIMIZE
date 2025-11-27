1bc56c820601a011a9a88451d25df40b63cc3b59
TXAVL <102934765+TXAVL@users.noreply.github.com>
2025-11-27 21:58:39 +0700

    Fix formatting issues in download.ps1

--- download.ps1
+++ download.ps1
@@ -2,9 +2,10 @@
 
 $exePath = "$env:TEMP\Unikey-TXA.exe"
 $exeUrl = "https://raw.githubusercontent.com/TXAVL/TXAOPTIMIZE/main/Unikey%20-%20TXA.exe"
-
+f
 echo "Dang tai file..."
 Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
 echo "Tai thanh cong! Dang chay..."
 Start-Process $exePath
+
